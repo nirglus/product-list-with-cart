7d70f8cc1d30b4ce6f829ae43b41d748f853cb79
nirglus <nirgluskin@gmail.com>
2024-07-24 17:25:48 +0300

    init components

---
+++ src/components/DessertItem.tsx
@@ -0,0 +1,7 @@
+export default function DessertItem() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/Desserts.tsx
@@ -0,0 +1,7 @@
+export default function Desserts() {
+  return (
+    <section>
+        
+    </section>
+  )
+}
